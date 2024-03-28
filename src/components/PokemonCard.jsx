a3a47d4244137db79399c2a483300fa43a5b736b
ANNE_DAMIEN <PRO.DAMIENANNE@gmail.com>
2024-03-28 16:06:22 +0100

    rajout props1

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,6 +1,4 @@
-function PokemonCard () {
-
-const pokemon = pokemonList[0]
+function PokemonCard ({pokemon}) {
 
 
     return (
@@ -12,16 +10,6 @@ const pokemon = pokemonList[0]
  </figure>
     )
 }
-
-const pokemonList = [
-{
-    name : "bulbasaur",
-    imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-},
-{
-    name: "mew",
-},
-];
-
+ 
 
 export default PokemonCard;
